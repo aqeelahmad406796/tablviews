f88b59294ad02c7c1dc14fe65815286bfa75ea01
Aqeel Ahmad <aqeelahmad@Aqeels-Mac.local>
2018-06-05 14:22:01 -0700

    first commit

---
+++ tablview/myTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableViewCell.swift
+//  tablview
+//
+//  Created by Aqeel Ahmad on 6/5/18.
+//  Copyright © 2018 Aqeel Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
